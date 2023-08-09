6372aa5735702baeb0abc1560a499307e5a7d5eb
h-orito <16453967+h-orito@users.noreply.github.com>
2023-08-09 23:46:20 +0900

    fix favicons

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -12,24 +12,24 @@ export default function Document() {
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/favicons/apple-touch-icon.png'
+          href='/chat-role-play/favicons/apple-touch-icon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/favicons/favicon-32x32.png'
+          href='/chat-role-play/favicons/favicon-32x32.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/favicons/favicon-16x16.png'
+          href='/chat-role-play/favicons/favicon-16x16.png'
         />
-        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link rel='manifest' href='/chat-role-play/favicons/site.webmanifest' />
         <link
           rel='mask-icon'
-          href='/favicons/safari-pinned-tab.svg'
+          href='/chat-role-play/favicons/safari-pinned-tab.svg'
           color='#5bbad5'
         />
         <meta name='msapplication-TileColor' content='#da532c' />
