4246ab31d6d0d37d0f5e8dbbbeb19f6515855cdb
h-orito <16453967+h-orito@users.noreply.github.com>
2024-02-19 22:12:54 +0900

    TypeError: fetch failed 対策

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
+// see https://zenn.dev/duo3/articles/dbb8115309059e
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first")
+
 const nextConfig = {
     basePath: '/chat-role-play',
     images: {
