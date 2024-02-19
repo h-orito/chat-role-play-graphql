998f086bf01cef50ebc45b20541c5166e90d5f50
h-orito <16453967+h-orito@users.noreply.github.com>
2024-02-19 22:18:08 +0900

    戻す

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
-// see https://zenn.dev/duo3/articles/dbb8115309059e
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first")
-
 const nextConfig = {
     basePath: '/chat-role-play',
     images: {
